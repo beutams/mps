4244d4ba55310e57dd0806c041334ab9f5c5ad55
hrz <260>
2025-06-19 18:24:36 +0800

    shopui

---
+++ Assets/Scripts/Game/Game/UI/ShopUI.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopUI : MonoBehaviour 
+{
+    public Transform shopContent;
+    public Transform info;
+
+    public Transform weapenContent;
+    protected List<Weapen> productList = new List<Weapen>();
+
+}
